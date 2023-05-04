43fad4d57b31de72fff4bfd135bf9b0b153e067b
ougrow0905 <outgrow0905@naver.com>
2023-05-04 18:09:09 +0900

    log tracer 5

--- src/main/java/com/springboot/advanced/ch2/v6/OrderRepositoryV6.java
+++ src/main/java/com/springboot/advanced/ch2/v6/OrderRepositoryV6.java
@@ -1,12 +1,8 @@
 package com.springboot.advanced.ch2.v6;
 
 import com.springboot.advanced.ch1.trace.LogTrace;
-<<<<<<< HEAD
+
 import com.springboot.advanced.ch2.v6.template.AbstractTemplate;
-=======
-import com.springboot.advanced.ch1.trace.TraceStatus;
-import com.springboot.advanced.ch2.template.AbstractTemplate;
->>>>>>> 438818ac4bcd8ad08140edf8a4d7967c357e60f7
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
